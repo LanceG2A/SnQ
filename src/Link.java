6e02f0ba07b356c1e587ec8b92fbc24e8fb1668e
LGA2 <lackerson@albany.edu>
2017-04-09 13:05:08 -0400

    Touch ups

--- src/Link.java
+++ src/Link.java
@@ -1,14 +1,14 @@
 
-public class Link {
-	Link next;
+public class Link { //Class to store each Link of a stack or queue
+	Link next;		//Fields to store color name and next link
 	String color;
 	
 	
-	public Link(String color){
+	public Link(String color){		
 		this.next = null;
 		this.color = color;
 	}
-	public Link(String color, Link next){
+	public Link(String color, Link next){//Constructor takes in data and next link
 		this.color = color;
 		this.next = next;
 	}
--- src/Queue.java
+++ src/Queue.java
@@ -1,27 +1,27 @@
 
-public class Queue {
+public class Queue {   // Fields to keep track of front and back of queue, as well as size
 	private Link head;
 	private Link tail;
 	private int size;
 	
-	public void enqueue(String color){
+	public void enqueue(String color){//Method to add a new Link to the queue
 		if(head == null){
-			Link newLink = new Link(color, null);
+			Link newLink = new Link(color, null);//If there is nothing, first Link is made
 			head = newLink;	
 			tail = newLink;
 			size++;
-			System.out.println("enqueue("+color+")");
+			System.out.println("enqueue("+color+")");//To show that an item is being added
 		}
 		else{
 			if(head.next == null){
-			Link newLink = new Link(color, null);
-			tail = newLink;
+			Link newLink = new Link(color, null); //If there is only one item in the queue
+			tail = newLink;							//Adds the new link in front of head
 			head.next = tail;
 			size++;
 			System.out.println("enqueue("+color+")");
 			}
 			else{
-				Link temp = head;
+				Link temp = head;  //iterates through the queue to add the new link to the end
 				while(temp.next != null){
 					temp = temp.next;
 				}
@@ -33,28 +33,28 @@ public class Queue {
 			}
 		}
 	}
-	public void dequeue(){
+	public void dequeue(){//Method to remove a link
 		if(head == null){
-			System.out.println("Empty List");
+			System.out.println("Empty List");//if its empty you can't remove anything
 		}
 		else{
 			Link temp = head;
-			System.out.println("dequeue("+temp.color+")");
-			head = temp.next;
+			System.out.println("dequeue("+temp.color+")");//set head to the next node which
+			head = temp.next;						//removes it by changing pointers
 			size--;
 	}
 	}
-	public Link peek(){
+	public Link peek(){//Views the head link without altering it
 		return head;
 	}
-	public void printList(){
+	public void printList(){//Prints the items in the queue in order
 		Link temp = head;
 		while(temp != null){
 			System.out.println(temp.color);
 			temp = temp.next;
 		}
 	}
-	public int getSize(){
+	public int getSize(){//returns size of queue
 		return size;
 	}
 
